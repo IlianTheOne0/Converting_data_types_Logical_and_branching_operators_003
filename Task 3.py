48d94cabf2cf2f97a494eec69b04303b7f622981
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-25 13:32:39 +0300

    Completed Task 3

---
+++ Task 3.py
@@ -0,0 +1,5 @@
+callCost = int(input('Call cost: '))
+operator = input('Your operator: ')
+subscribersOperator = input('Operator of the subscriber: ')
+
+print(f'The cost of a call from the operator {operator} to the operator of the subscriber {subscribersOperator}, you need {callCost} units of monetary currency.')
